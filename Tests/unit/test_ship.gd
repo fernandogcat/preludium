5b64a9f683f973ad9d1442c26345e2bb485d602b
fegabe <fegabe@gmail.com>
2024-12-12 16:13:08 +0100

    🧪 Add tests for ship

---
+++ Tests/unit/test_ship.gd
@@ -0,0 +1,37 @@
+extends GutTest
+
+var sut: Ship
+
+var input_sender = InputSender.new(Input)
+
+func before_each():
+    sut = Ship.new()
+
+func after_each():
+    sut.free()
+
+func test_ship_starts_without_shots():
+    assert_eq(sut.num_shots, 0)
+
+
+func test_perform_shoot_increases_num_shots():
+    sut.perform_shoot()
+
+    assert_gt(sut.num_shots, 0)
+
+func test_check_shoot_no_input_no_shoots():
+    input_sender.release_all()
+    input_sender.clear()
+
+    sut.check_shoot_input()
+
+    assert_eq(sut.num_shots, 0)
+
+func test_check_shoot_input_increases_num_shoots():
+    input_sender.release_all()
+    input_sender.clear()
+
+    input_sender.action_down("shoot")
+    sut.check_shoot_input()
+
+    assert_gt(sut.num_shots, 0)
