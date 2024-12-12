50d6d72568dd307e01464f62d269bea7aa659f18
fegabe <fegabe@gmail.com>
2024-12-12 17:22:59 +0100

    ♻️ Rename ship methods

--- Components/SampleGame/GameLogic/GameLogic.gd
+++ Components/SampleGame/GameLogic/GameLogic.gd
@@ -7,8 +7,8 @@ func _ready():
 	ship.ship_shot.connect(_on_ship_shot)
 
 func update_physics(delta):
-	ship.check_move(delta)
-	ship.check_shoot_input()
+	ship.check_input_move(delta)
+	ship.check_input_shoot()
 
 func _on_ship_shot(start_global_position: Vector2, direction: Vector2):
 	var bullet := Bullet.build().setup(start_global_position, direction)
--- Tests/unit/test_ship.gd
+++ Tests/unit/test_ship.gd
@@ -23,15 +23,15 @@ func test_check_shoot_no_input_no_shoots():
     input_sender.release_all()
     input_sender.clear()
 
-    sut.check_shoot_input()
+    sut.check_input_shoot()
 
     assert_eq(sut.num_shots, 0)
 
-func test_check_shoot_input_increases_num_shoots():
+func test_check_input_shoot_increases_num_shoots():
     input_sender.release_all()
     input_sender.clear()
 
     input_sender.action_down("shoot")
-    sut.check_shoot_input()
+    sut.check_input_shoot()
 
     assert_gt(sut.num_shots, 0)
