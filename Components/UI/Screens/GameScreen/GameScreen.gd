9b4a3c502e34bbd6fbcf47f73cdca4516aaddc47
fegabe <fegabe@gmail.com>
2024-12-13 15:21:22 +0100

    💄 Add a background and change modal color

--- Components/UI/Screens/GameScreen/GameScreen.gd
+++ Components/UI/Screens/GameScreen/GameScreen.gd
@@ -2,6 +2,7 @@ class_name GameScreen
 extends BaseScreen
 
 @onready var game_logic := %GameLogic
+@onready var pause_button := %PauseButton
 @onready var pause_modal_screen := %PauseModalScreen
 
 func _input(event: InputEvent):
@@ -23,6 +24,7 @@ func _check_ui_actions_input(event: InputEvent) -> bool:
 
 func show_modal_screen(modal_screen: BaseModalScreen, show_modal: bool):
 	super.show_modal_screen(modal_screen, show_modal)
+	pause_button.visible = !show_modal
 	game_logic.pause_game(show_modal)
 
 func _on_pause_button_pressed():
