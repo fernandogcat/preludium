1fcad2b3a21faa1458b87d69597c6d6c33eb925a
fegabe <hi@fernandog.cat>
2025-07-12 18:07:58 +0200

    ✨ Improve output check of tests to handle new ways of tests failing

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-GUT_OUTPUT=$(godot --headless --path . -s addons/gut/gut_cmdln.gd -gexit)
+GUT_OUTPUT=$(godot --headless --path . -s addons/gut/gut_cmdln.gd -gexit 2>&1 | tee /dev/stderr)
 
-echo -e "$GUT_OUTPUT"
-
-# In order to really check if tests were successful, we need to check for the text "All tests passed" 
-# because if there are skipped tests, the exit code will be 0 and considered as success
-if [[ $GUT_OUTPUT == *"All tests passed"* ]]; then
+if [[ $GUT_OUTPUT == *"because it does not extend GutTest"* ]]; then
+  echo -e "\e[31ma) Not all test files were executed. Tests failed! 😭\e[0m"
+  exit 1
+elif [[ $GUT_OUTPUT == *"SCRIPT ERROR"* ]]; then
+  echo -e "\e[31mb) Some script errors occurred. Tests failed! 😭\e[0m"
+  exit 1
+elif [[ $GUT_OUTPUT != *"All tests passed"* ]]; then
+  echo -e "\e[31mc) Not all tests passed. Tests failed! 😭\e[0m"
+  exit 1
+else
   echo -e "\e[32mTests passed! ✅\e[0m"
   exit 0
-else
-  echo -e "\e[31mTests failed! 😭\e[0m"
-  exit 1
 fi
